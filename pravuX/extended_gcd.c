2a184514b607106ef9a3100e43662d5f4b65011e
Prabesh Subedi <prabesh.sb@gmail.com>
2023-01-20 23:27:30 +0545

    Add extended gcd

---
+++ pravuX/extended_gcd.c
@@ -0,0 +1,36 @@
+// To find the multiplicative inverse of a number 'b' in the set 'Zn'
+#include <stdio.h>
+
+int inverse(int b, int n) {
+    int r, r1, r2, t, t1, t2, i, q;
+    // initilaize all variables
+    i = 0;
+    t1 = 0;
+    t2 = 1;
+    r1 = n;
+    r2 = b;
+    // implement the extended gcd algorithm
+    while (r2 > 0) {
+        q = r1 / r2;
+        r = r1 % r2;
+        r1 = r2, r2 = r;
+
+        t = t1 - q * t2;
+        t1 = t2, t2 = t;
+    }
+    if (r1 == 1) // inverse exists
+        // inverse is t1 mod n
+        i = t1 < 0 ? n + t1 : t1 % n;
+    return i;
+}
+
+int main() {
+    int n, b, i;
+    puts("Enter value n for Zn");
+    scanf("%d", &n);
+    printf("Enter value b to find multiplicate inverse in Z%d\n", n);
+    scanf("%d", &b);
+    i = inverse(b, n);
+    printf("The inverse of %d in Z%d is %d.", b, n, i);
+    return 0;
+}
