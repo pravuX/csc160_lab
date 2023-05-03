12c3f2ecd5fec6436980a5f5706f4ac4a272be5a
Prabesh Subedi <prabesh.sb@gmail.com>
2023-05-03 09:51:01 +0545

    add series printing

--- pravuX/for_practical_exam/scratch.c
+++ pravuX/for_practical_exam/scratch.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
 int main() {
+  int i, j, k;
+  k = 3;
+  for (i = 1, j = 2; i <= 10; i++, j+=k, k+=2) {
+    printf("%d/%d + ", i, j);
+  }
+  printf("\b\b ");
   return 0;
 }
