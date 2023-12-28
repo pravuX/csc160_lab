791744742632ab5492a98bc40c4f7ae316e1978f
Prabesh Subedi <prabesh.sb@gmail.com>
2023-12-28 11:39:20 +0545

    Refactor binary to decimal program and remove bin files

--- pravuX/binary_to_decimal.c
+++ pravuX/binary_to_decimal.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include <math.h>
-#define size 10 // maximum number of bits in a binary number
+#define size 8 // maximum number of bits in a binary number
 
-int toDecimal(int *bit_string) {
-  int num, i;
+int to_decimal(char *bit_string) {
+  int num, i, pos;
   num = 0;
   i = size - 1;
+  pos = 0;
   while (i >= 0) {
-    num += bit_string[i] * (int) pow(2, size-i-1);
+    if (bit_string[i]) {
+      num += (bit_string[i] - 48) * (int) pow(2, pos);
+      pos++;
+    }
     i--;
   }
+  putchar('\n');
   return num;
 }
 
 int main() {
-
+  char bit_string[size] = {0};
+  int dec;
+  fscanf(stdin, "%s", bit_string);
+  dec = to_decimal(bit_string);
+  return 0;
 }
--- pravuX/for_practical_exam/to_binary.c
+++ pravuX/for_practical_exam/to_binary.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#define N 8
 void to_binary_str(int *bin_str, int dec, int n) {
   // masking with 1(bitwise ANDing) to obtain the lsb
   // shifting right to obtain each bit
@@ -13,5 +15,11 @@ void to_binary_str(int *bin_str, int dec, int n) {
 }
 
 int main() {
+  int bin_str[N], dec;
+  puts("Enter decimal number");
+  scanf("%d", &dec);
+  to_binary_str(bin_str, dec, N);
+  puts("Binary number is");
+  for (int i = 0; i < N; i++) printf("%d", bin_str[i]);
   return 0;
 }
